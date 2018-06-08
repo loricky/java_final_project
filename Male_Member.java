f2bf3d04235c4786e86048bc2fbd094b660c6667
loricky <rickylo841210@yahoo.com.tw>
2018-06-08 11:52:43 +0800

    Update Male_Member.java

--- Male_Member.java
+++ Male_Member.java
@@ -1,11 +1,30 @@
 public class Male_Member extends Member{
 	private int salary; //薪水
-	private boolean car; //有車?
+	//private boolean car; //有車?
 	
 	public Male_Member(){
-	   setName("");
-	   set
-		
-		
+		setFull_Name("");
+                setHeight(0.0);
+	        setWeight(0.0);
+	        setAge(0);
+		setSalary(0);
+		//setCar();		
+	}
+	public Male_Member(String f, float h, float w, int a, int s){
+		setFull_Name(f);
+		setHeight(h);
+		setWeight(w);
+		setAge(a);
+		setSalary(s);
+	}
+	public void setSalary(int s){
+		if(s < 0){
+			System.out.println("salary can't be under 0");
+		}else{
+			salary = s;
+		}
+	}
+	public int getSalary(){
+		return salary;
 	}
 }
